83a16f4d9a16738c187bad8a27cfca92bc8a8273
doujiang-zheng <1825597835@qq.com>
2021-08-11 11:18:55 +0800

    Bi-58: 1958.

--- bi-58/1958.py
+++ bi-58/1958.py
@@ -1,7 +1,45 @@
 class Solution:
-    def checkMove(self, board: List[List[str]], rMove: int, cMove: int,
+    def checkLineLegal(self, line):
+        if len(line) < 3:
+            return False
+
+        for color in line:
+            if color == '.':
+                return False
+
+        if line[0] != line[-1]:
+            return False    
+        
+        for i in range(1, len(line) - 1):
+            if line[i] == line[0]:
+                return False
+
+        return True
+
+    def isValid(self, board, i, j):
+        x = len(board)
+        y = len(board[0])
+        return (0 <= i < x) and (0 <= j < y)
+        
+
+    def checkMove(self, board, rMove: int, cMove: int,
                   color: str) -> bool:
-        pass
+        board[rMove][cMove] = color
+        dxs = [0, 1, 0, -1, -1, 1, 1, -1]
+        dys = [1, 0, -1, 0, -1, -1, 1, 1]
+
+        for dx, dy in zip(dxs, dys):
+            line = []
+            for step in range(len(board)):
+                endx = rMove + dx * step 
+                endy = cMove + dy * step
+                if not self.isValid(board, endx, endy):
+                    break
+                
+                line.append(board[endx][endy])
+                if self.checkLineLegal(line):
+                    return True
+        return False
 
 
 if __name__ == '__main__':
