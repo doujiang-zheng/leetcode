bfea468e7b8e16dd052395e284664fb7eab3a92b
doujiang-zheng <1825597835@qq.com>
2021-08-12 11:11:40 +0800

    Binary-search: two pointer solution for 240 is optimal.

--- binary-search/240.py
+++ binary-search/240.py
@@ -2,46 +2,20 @@ from typing import Dict, Tuple, List
 
 
 class Solution:
-    def bisect_right(self, arr, target):
-        lo = 0
-        hi = len(arr)
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if arr[mid] <= target:
-                lo = mid + 1
-            else:
-                hi = mid
-        return lo
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Two pointer
         m = len(matrix)
-        if m <= 0:
-            return False
         n = len(matrix[0])
-        if n <= 0:
-            return False
-        if m == 1 and n == 1:
-            return matrix[0][0] == target
-        if m <= 0 or n <= 0:
-            return False
 
-        col0 = [row[0] for row in matrix]
-        row_idx = self.bisect_right(col0, target) - 1
-        if row_idx < 0 or row_idx >= m:
-            return False
-        col_idx = self.bisect_right(matrix[row_idx], target) - 1
-        if col_idx < 0 or col_idx >= n:
-            return False
-        # matrix[row_idx][0] <= target < matrix[row_idx+1][0]
-        # matrix[row_idx][col_idx] <= target < matrix[row_idx][col_idx+1]
-        if matrix[row_idx][col_idx] == target:
-            return True
-
-        upper_right = [row[col_idx + 1:] for row in matrix[:row_idx]]
-        lower_left = [row[:col_idx + 1] for row in matrix[row_idx + 1:]]
-        if self.searchMatrix(upper_right, target) or self.searchMatrix(
-                lower_left, target):
-            return True
+        i, j = 0, n - 1
+        while (0 <= i < m) and (0 <= j < n):
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
         return False
 
 
